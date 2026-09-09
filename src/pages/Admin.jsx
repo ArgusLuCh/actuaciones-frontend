c7a034fe4eec0b4fde4ec73498847a5750929a34
Argus <agustinluch29@gmail.com>
2026-09-09 11:53:30 -0300

    Se fixea el useNavigate repetido

--- src/pages/Admin.jsx
+++ src/pages/Admin.jsx
@@ -1,6 +1,5 @@
 import { useState, useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
-import { useNavigate } from 'react-router-dom'
 
 const API = "https://actuaciones-backend-production.up.railway.app"
 
